573caa42163526f3743154760a8b43c9d5f7b415
johnc1to <59821507+johnc1to@users.noreply.github.com>
2022-11-08 13:17:32 -0500

    Update OwnerServiceUpdate.java

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceUpdate.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceUpdate.java
@@ -18,54 +18,54 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.tecsup.petclinic.entities.Owner;
 import com.tecsup.petclinic.entities.Pet;
 import com.tecsup.petclinic.exception.PetNotFoundException;
 
 import com.tecsup.petclinic.entities.Pet;
-
+@SpringBootTest
 public class OwnerServiceUpdate {
+	private static final Logger logger = LoggerFactory.getLogger(OwnerServiceTestcreate.class);
+	private OwnerService OwnerService;
+	@Test
 	public void testUpdatePet() {
-
+		 
 		int Id=1;
-		String first_name="John",
-		String last_name="Baldeon",
+		String first_name="John";
+		String last_name="Baldeon";
 		String city ="Lima";
 		String address="johnb";
-		String telephone="987654321"
+		String telephone="987654321";
 		
-		int Id=2;
-		String first_name="John",
-		String last_name="Baldeon",
-		String city ="Lima";
-		String address="johnb";
-		String telephone="987654321"
+		
 
 		Owner owner = new Owner(Id,first_name,last_name,city,address,telephone);
 
 		// Create record
 		logger.info(">" + owner);
-		Owner ownerCreated = ownerService.create(owner);
+		Owner ownerCreated = OwnerService.create(owner);
 		logger.info(">>" + ownerCreated);
 
-		create_id = ownerCreated.getId();
+		long create_id = ownerCreated.getId();
 
 		// Prepare data for update
-		ownerCreated.setFirst_name();
-		ownerCreated.setLast_name();
-		ownerCreated.setCity();
-		ownerCreated.setAddress();
-		ownerCreated.setTelephone();
+		ownerCreated.setFirst_name(first_name);
+		ownerCreated.setLast_name(last_name);
+		ownerCreated.setCity(city);
+		ownerCreated.setAddress(address);
+		ownerCreated.setTelephone(telephone);
 
 		// Execute update
-		Owner upgradeOwner = ownerService.update(OwnerCreated);
+		Owner upgradeOwner = OwnerService.update(ownerCreated);
 		logger.info(">>>>" + upgradeOwner);
 
 		//        ACTUAL       EXPECTED
 		assertThat(create_id ,notNullValue());
-		assertThat(upgradeOwner.getId(), is(create_Id));
+		assertThat(upgradeOwner.getId(), is(create_id));
 		assertThat(upgradeOwner.getFirst_name(), is(first_name));
 		assertThat(upgradeOwner.getLast_name(), is(last_name));
-		assertThat(upgradeOwner.getcity(), is(city));
+		assertThat(upgradeOwner.getCity(), is(city));
 		assertThat(upgradeOwner.getTelephone(), is(telephone));
 	}
 }
+
