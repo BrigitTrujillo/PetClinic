de8202179da0b712b5e283723607fe7e691d9398
BrigitTrujillo <112431450+BrigitTrujillo@users.noreply.github.com>
2022-11-08 11:35:03 -0500

    Add files via upload

---
+++ src/main/java/com/tecsup/petclinic/exception/OwnerNotFoundException.java
@@ -0,0 +1,16 @@
+package com.tecsup.petclinic.exception;
+
+public class OwnerNotFoundException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public OwnerNotFoundException(String message) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
